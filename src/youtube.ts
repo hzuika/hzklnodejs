5e5f43b1e66e756b50e696ef71bf08f6d201834a
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-08-01 23:05:39 +0900

    separate youtube namespace

--- src/youtube.ts
+++ src/youtube.ts
@@ -1,1 +1,22 @@
+import { Opaque } from "type-fest";
+export namespace Youtube {
+  type VideoId = Opaque<string, "VideoId">;
+  const VideoId = {
+    new: (id: string): VideoId => {
+      return id as VideoId;
+    },
 
+    validate: (id: string): boolean => {
+      return id.length === 11;
+    },
+  };
+  type ChannelId = Opaque<string, "ChannelId">;
+  type UploadPlaylistId = Opaque<string, "UploadPlaylistId">;
+  type RegularPlaylistId = Opaque<string, "RegularPlaylistId">;
+  export class Api {
+    private readonly apiKey: string;
+    constructor(apiKey: string) {
+      this.apiKey = apiKey;
+    }
+  }
+}
